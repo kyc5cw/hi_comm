858819233348ff1bf2ed7dd61b18371ba2a1ad91
Prelang Builder <builder@prelang.com>
2014-06-25 20:24:00 +0000

    Generated scaffolding for model 'Post' via Rails

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :board_id, :title, :content, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+
   resources :groups
 
   devise_for :admin_users, ActiveAdmin::Devise.config
